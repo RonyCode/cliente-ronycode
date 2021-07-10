f90cd62c7abe637339ce5e4ca93226815580ffc4
ronycode <170286Para>
2021-07-10 00:25:39 -0300

    form login 0.4

--- src/Components/Pages/Login/LoginForm.js
+++ src/Components/Pages/Login/LoginForm.js
@@ -2,18 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "../../UserStorage";
 import styles from "./LoginForm.module.css";
-
 function LoginForm() {
   const [email, setEmail] = React.useState("");
+  const [imgReveal, setimgReveal] = React.useState(
+    "https://i.ibb.co/BzZZ8wZ/3844443-disable-eye-inactive-see-show-view-watch-110296.png"
+  );
   const [pass, setPass] = React.useState("");
   const [revelPass, setRevelPass] = React.useState("password");
-  const { userLogin } = React.useContext(UserContext);
+  const { userLogin, src } = React.useContext(UserContext);
 
   const formData = new FormData();
   formData.append("email", email);
   formData.append("pass", pass);
-
-  // const btnrevel = document.addEventListener("input");
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -21,22 +21,28 @@ function LoginForm() {
   }
 
   const handleRevelPass = () => {
-    revelPass === "password" ? setRevelPass("text") : setRevelPass("password");
+    if (revelPass === "password") {
+      setRevelPass("text");
+      setimgReveal(
+        "https://i.ibb.co/X5SYD3x/3844441-eye-see-show-view-watch-110305.png"
+      );
+    } else {
+      setRevelPass("password");
+      setimgReveal(
+        "https://i.ibb.co/BzZZ8wZ/3844443-disable-eye-inactive-see-show-view-watch-110296.png"
+      );
+    }
   };
 
   return (
     <div className={styles.loginForm}>
       <h1 className={styles.tittle}> Login </h1>
       <div className={styles.container}>
-        <div className={styles.columnLeft + " animeLeft"}></div>
+        <div className={styles.columnLeft + " animeRight"}></div>
+        <div>
+          <h2 className={styles.subtittle + " animeRight"}>Espaço Educar</h2>
+        </div>
         <div className={styles.columnRight + " animeLeft"}>
-          <div className={styles.imgUser}>
-            <img
-              src="https://i.ibb.co/S7r0zFb/novo-logo-composto.png"
-              alt="novo-logo-composto"
-              border="0"
-            />
-          </div>
           <form onSubmit={handleSubmit} className={styles.form}>
             <div className={styles.div_input}>
               <label htmlFor="email" className={styles.label}>
@@ -56,43 +62,46 @@ function LoginForm() {
               <label htmlFor="pass" className={styles.label}>
                 Senha:
               </label>
-              <input
-                id="inputPass"
-                className={styles.input}
-                placeholder="&#61475;"
-                type={revelPass}
-                name="pass"
-                value={pass || ""}
-                onChange={({ target }) => setPass(target.value)}
-              />
-              <span className={styles.revelPass} onClick={handleRevelPass}>
-                &copy;
-              </span>
+              <div className={styles.div_input_pass}>
+                <input
+                  className={styles.input}
+                  // placeholder="&#61475;"
+                  type={revelPass}
+                  name="pass"
+                  value={pass || ""}
+                  onChange={({ target }) => setPass(target.value)}
+                />
+                <img
+                  onClick={handleRevelPass}
+                  className={styles.img_icon_reveal}
+                  src={imgReveal}
+                  alt="eye-button"
+                  border="0"
+                />
+              </div>
             </div>
 
             <button className={styles.button}>Entrar</button>
           </form>
-        </div>
-        <div>
-          <h2 className={styles.subtittle + " animeRight"}>Espaço Educar</h2>
-        </div>
-        <div className={styles.loginFolder + " animeRight"}>
-          <div className={styles.divCheckbox}>
-            <input
-              className={styles.checkbox}
-              type="checkbox"
-              id="save_login"
-              name="save_login"
-            />
-            <label className={styles.labelCheckbox} htmlFor="save_login">
-              relembrar meu login
-            </label>
+
+          <div className={styles.loginFolder}>
+            <div className={styles.divCheckbox}>
+              <input
+                className={styles.checkbox}
+                type="checkbox"
+                id="save_login"
+                name="save_login"
+              />
+              <label className={styles.labelCheckbox} htmlFor="save_login">
+                relembrar meu login
+              </label>
+            </div>
+            <Link className={styles.linkRecover} to="/login/recuperar">
+              Esqueceu sua senha?
+            </Link>
           </div>
-          <Link className={styles.linkRecover} to="/login/recuperar">
-            Esqueceu sua senha?
-          </Link>
         </div>
-        <Link className={styles.linkSign} to="/login/cadastrar">
+        <Link className={styles.linkSign + " animeLeft"} to="/login/cadastrar">
           Não tem login? Cadastre-se
         </Link>
       </div>
